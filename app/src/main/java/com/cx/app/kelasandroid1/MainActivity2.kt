1af6d14cec635dc8a8a0be6983c9a0a783eaad3b
Kasmadi <kasmadi1717@gmail.com>
2021-06-23 17:12:18 +0700

    get data from intent

--- app/src/main/java/com/cx/app/kelasandroid1/MainActivity2.kt
+++ app/src/main/java/com/cx/app/kelasandroid1/MainActivity2.kt
@@ -2,9 +2,14 @@ package com.cx.app.kelasandroid1
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity2 : AppCompatActivity() {
+
+    private var id:TextView? =null
+    private var name:TextView? = null
+
     private val TAG = "MainActivity2"
 
     override fun onStart() {
@@ -15,7 +20,16 @@ class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        id = findViewById(R.id.tvId)
+        name = findViewById(R.id.tvName)
         Log.i(TAG, "onCreate: ")
+
+        val textId = intent.getIntExtra("id", 0)
+        val textName = intent.getStringExtra("name")
+
+        id?.text = textId.toString()
+        name?.text = textName
+
     }
 
     override fun onResume() {
